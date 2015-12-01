49ab07bf2a698e93e3c1297d24855026e9c5162d
yama@muteki <yama@muteki.com>
2015-12-02 02:02:25 +0900

    Improved distribution index page status.

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -1,5 +1,8 @@
 class Distribution < ActiveRecord::Base
   has_many :respondents
+  has_many :sent_respondents, -> { where.not first_sent_at: nil }, class_name: 'Respondent'
+  has_many :accessed_respondents, -> { where.not accessed_at: nil }, class_name: 'Respondent'
+  has_many :answered_respondents, -> { where.not answered_at: nil }, class_name: 'Respondent'
 
   validates :name, presence: true
 end
