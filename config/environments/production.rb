fe28fdd08969bf109433e5b92cffdf82ba62add1
yama@muteki <yama@muteki.com>
2015-12-12 18:52:21 +0900

    Add SES Settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,15 +63,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "smart-inquiries.herokuapp.com", :protocol => 'https' }
+  config.action_mailer.default_url_options = { host: "smart-inquiries-dev.elasticbeanstalk.com", :protocol => 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
+    :address => 'email-smtp.us-east-1.amazonaws.com',
     :port    => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com'
+    :user_name => ENV['SES_USERNAME'],
+    :password => ENV['SES_PASSWORD'],
+    :domain => 'amazonaws.com'
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
