d1ab2b516543fbb55aafabf001b5cee5c8947f4e
yama@muteki <yama@muteki.com>
2015-12-13 14:43:48 +0900

    Change host name.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "smart-inquiries-dev.elasticbeanstalk.com", :protocol => 'http' }
+  config.action_mailer.default_url_options = { host: "smart-inquiries.azurewebsites.net", :protocol => 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => 'email-smtp.us-east-1.amazonaws.com',
